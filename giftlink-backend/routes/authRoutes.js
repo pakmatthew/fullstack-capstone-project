8f1faaebc88f2fc4e48133c94a18d48da60579c8
pakmatthew <pakfrontend0226@gmail.com>
2025-09-13 02:45:17 -0400

    fix: lint

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const dotenv = require('dotenv');
 const pino = require('pino');
